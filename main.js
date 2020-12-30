ba7d88a3b4e8455e961df073880c85a3b64ff6b2
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2020-12-29 21:07:22 -0800

    v2

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
  const api = {
-     key: "",
-     baseUrl: "http://"
+     key: "b114c682f8d346cf22ad132fef1710b8",
+     base: "http://api.openweathermap.org/data/2.5/"
  }
 
  const searchbox = document.querySelector(".search-box");
  searchbox.addEventListener("keypress", setQuery);
 
  function setQuery(evt) {
-     getResults(searchbox.value);
-     console.log("searchbox.value");
+    if(evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+ }
+
+ function getResults(query) {
+     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+     .then(weather => {
+         return weather.json();
+     }).then(displayResults);
+ }
+
+ function displayResults (weather) {
+     console.log(weather);
+     let city = document.querySelector('.location .city');
+     city.innerText = `${weather.name}, ${weather.sys.country}`;
  }
